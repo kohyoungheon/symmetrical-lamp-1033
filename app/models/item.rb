bd8a9ed1d05c752ac6f392c48f8796ea375510d4
Young Koh <youngheon6@gmail.com>
2023-04-10 10:51:13 -0600

    fix: refactor

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   has_many :customer_items
   has_many :customers, through: :customer_items
 
+  def total_buyers
+    customers.count
+  end
+
+  def get_market_name
+    supermarket.name
+  end
 end
